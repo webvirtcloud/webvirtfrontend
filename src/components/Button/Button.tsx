c150e297349c79c7d8d414da7858b0263da50a2f
Roman Slonov <destabiliseus@gmail.com>
2022-10-31 17:15:37 +0800

    Added loading, disabled state for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,20 +3,23 @@ import tw from 'twin.macro';
 
 interface Props {
   type?: 'submit' | 'button';
+  loading?: boolean;
+  disabled?: boolean;
   children: ReactNode;
 }
 
 // eslint-disable-next-line react/display-name
 const Button = forwardRef<HTMLButtonElement, Props>(
-  ({ children, type = 'button', ...rest }, ref): JSX.Element => {
+  ({ children, type = 'button', loading, disabled, ...rest }, ref): JSX.Element => {
     return (
       <button
         ref={ref}
         type={type}
+        disabled={disabled || loading}
         css={tw`h-12 text-sm font-bold bg-cyan-500 hover:bg-cyan-700 transition-colors rounded-xl px-4`}
         {...rest}
       >
-        {children}
+        {loading ? <span>Loading...</span> : children}
       </button>
     );
   },
