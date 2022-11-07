b4a3ef5ee8b9503519fea3b0765b194504caa2d5
Roman <destabiliseus@gmail.com>
2022-11-07 14:15:42 +0800

    Added user menu

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,8 +1,9 @@
 import ArrowDown from './ArrowDown';
 import Elipsis from './Elipsis';
+import Logout from './Logout';
 import Pause from './Pause';
 import Play from './Play';
 import ServerList from './ServerList';
 import Settings from './Settings';
 
-export { ArrowDown, Elipsis, Pause, Play, ServerList, Settings };
+export { ArrowDown, Elipsis, Logout, Pause, Play, ServerList, Settings };
--- src/components/Switcher/Switcher.tsx
+++ src/components/Switcher/Switcher.tsx
@@ -1,29 +1,77 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import tw from 'twin.macro';
 
 import type { Profile } from '@/api/account';
-import { ArrowDown } from '@/components/Icons';
+import { ArrowDown, Logout, Settings } from '@/components/Icons';
 
 const Switcher = ({ profile }: { profile: Profile }): JSX.Element => {
   const [isOpen, toggle] = useState(false);
-  return (
-    <button
-      onClick={() => toggle(true)}
-      type="button"
-      css={tw`flex items-center justify-between text-left bg-black/20 hover:bg-black/25 transition-opacity rounded-full py-2 px-3`}
-    >
-      <div css={tw`flex items-center space-x-3`}>
-        <figure css={tw`flex-shrink-0 w-8 h-8 bg-cyan-500 rounded-full`}>
-          {profile && profile.email[0].toUpperCase()}
-        </figure>
-        <div>
-          <h4 css={tw`font-bold`}>Basic</h4>
-          <p css={tw`text-white/50 text-xs`}>1488 VMs</p>
-        </div>
-      </div>
+  const navigate = useNavigate();
+  const location = useLocation();
 
-      <ArrowDown />
-    </button>
+  const handleLogout = () => {
+    window.localStorage.removeItem('token');
+
+    navigate('/sign-in');
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      toggle(false);
+    }
+  }, [location]);
+
+  return (
+    <div css={tw`relative`}>
+      <button
+        onClick={() => toggle(!isOpen)}
+        type="button"
+        css={tw`flex items-center justify-between text-left bg-black/20 hover:bg-black/25 transition-opacity rounded-full py-2 px-3`}
+      >
+        <div css={tw`flex items-center space-x-2`}>
+          <figure
+            css={tw`flex-shrink-0 flex items-center justify-center font-bold w-8 h-8 bg-cyan-500 rounded-full`}
+          >
+            {profile && profile.email[0].toUpperCase()}
+          </figure>
+          <div>
+            <h4 css={tw`font-bold`}>
+              {profile?.first_name} {profile?.last_name}
+            </h4>
+            {/* <p css={tw`text-white/50 text-xs`}></p> */}
+          </div>
+        </div>
+
+        <ArrowDown />
+      </button>
+      {isOpen && (
+        <ul
+          css={tw`bg-white/10 absolute bottom-[calc(100% + 8px)] left-0 right-0 rounded-xl p-2`}
+        >
+          <li>
+            <Link
+              css={tw`flex items-center w-full text-left p-2 hover:bg-black/20 rounded space-x-2`}
+              to="/settings"
+            >
+              <Settings />
+              <span css={tw`font-bold`}>Settings</span>
+            </Link>
+          </li>
+          <li css={tw`border-b border-black/30 my-1`}></li>
+          <li>
+            <button
+              css={tw`flex items-center w-full text-left p-2 hover:bg-black/20 rounded space-x-2`}
+              type="button"
+              onClick={() => handleLogout()}
+            >
+              <Logout />
+              <span css={tw`font-bold`}>Logout</span>
+            </button>
+          </li>
+        </ul>
+      )}
+    </div>
   );
 };
 
