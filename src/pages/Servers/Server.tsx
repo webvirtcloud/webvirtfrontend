e4567215d47e22f31fb018319f28502e94f87b2c
Roman <destabiliseus@gmail.com>
2022-11-08 22:48:43 +0800

    Fixed back button

--- src/pages/Servers/Server.tsx
+++ src/pages/Servers/Server.tsx
@@ -1,18 +1,21 @@
 import 'twin.macro';
 
+import { useAtomValue } from 'jotai';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import tw from 'twin.macro';
 
 import type { Server } from '@/api/servers';
 import { getServer } from '@/api/servers';
+import { useProjectStore } from '@/store/project';
 
 const ServerPage = (): JSX.Element => {
-  const { uuid } = useParams();
+  const { suuid } = useParams();
   const [server, setServer] = useState<Server>();
+  const project = useAtomValue(useProjectStore);
 
   const fetchServer = async () => {
-    const response = await getServer(uuid);
+    const response = await getServer(suuid as string);
 
     setServer(response.server);
   };
@@ -26,7 +29,7 @@ const ServerPage = (): JSX.Element => {
       {server ? (
         <>
           <header>
-            <Link css={tw`inline-block mb-4`} to="/servers">
+            <Link css={tw`inline-block mb-4`} to={`/projects/${project?.uuid}/servers`}>
               Back to list
             </Link>
             <h1 css={tw`text-2xl font-bold mb-8`}>{server.name}</h1>
