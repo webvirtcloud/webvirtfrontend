2bc162e8998c3abe2242c67a26a013ee86d056f5
Roman <destabiliseus@gmail.com>
2022-11-19 11:36:03 +0800

    Fixed logo size, added shadow for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,13 +13,15 @@ const Navbar = (): JSX.Element => {
   const project = useAtomValue(useProjectStore);
 
   return (
-    <nav css={tw`bg-base sticky top-0 left-0 right-0 flex flex-col justify-between`}>
+    <nav
+      css={tw`bg-base sticky top-0 left-0 right-0 flex flex-col justify-between shadow-sm`}
+    >
       <div css={tw`container mx-auto p-4`}>
         <div css={tw`flex items-center justify-between`}>
           <div css={tw`flex items-center space-x-4 mb-4`}>
             <Link css={tw`inline-flex items-center justify-center space-x-4`} to="/">
               <img
-                css={tw`w-10`}
+                css={tw`w-8`}
                 src={new URL('/src/assets/images/logo.svg', import.meta.url).href}
                 alt="Logotype"
               />
