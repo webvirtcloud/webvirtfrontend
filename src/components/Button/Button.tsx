f235f63d0b901c0fd98f12c88e629dcca08e052f
Roman <destabiliseus@gmail.com>
2022-11-19 17:04:07 +0800

    Added sizes for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,7 @@ import tw from 'twin.macro';
 
 interface Props {
   type?: 'submit' | 'button';
+  size?: 'sm' | 'md' | 'lg' | 'xl';
   loading?: boolean;
   disabled?: boolean;
   children: ReactNode;
@@ -10,21 +11,32 @@ interface Props {
 
 // eslint-disable-next-line react/display-name
 const Button = forwardRef<HTMLButtonElement, Props>(
-  ({ children, type = 'button', loading, disabled, ...rest }, ref): JSX.Element => {
+  (
+    { children, type = 'button', size = 'xl', loading, disabled, ...rest },
+    ref,
+  ): JSX.Element => {
     return (
       <button
         ref={ref}
         type={type}
         disabled={disabled || loading}
         css={[
-          tw`h-12 text-sm font-bold transition-colors rounded-md px-4`,
+          tw`text-sm font-bold transition-colors rounded-md`,
+          size === 'sm'
+            ? tw`h-6 px-1.5`
+            : size === 'md'
+            ? tw`h-8 px-2`
+            : size === 'lg'
+            ? tw`h-10 px-3`
+            : size === 'xl'
+            ? tw`h-12 px-4`
+            : tw``,
           disabled || loading
             ? tw`cursor-not-allowed bg-white/10`
             : tw`bg-cyan-500 hover:bg-cyan-700`,
         ]}
         {...rest}
       >
-        {disabled}
         {loading ? <span>Loading...</span> : children}
       </button>
     );
