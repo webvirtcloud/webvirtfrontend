727d3d20601163fd46ec127afd782e8612297102
Roman <destabiliseus@gmail.com>
2022-11-19 17:05:43 +0800

    Refactored project selector

--- src/components/ProjectSelector/ProjectSelector.tsx
+++ src/components/ProjectSelector/ProjectSelector.tsx
@@ -4,7 +4,8 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import tw, { css } from 'twin.macro';
 
 import type { Project } from '@/api/projects';
-import { CheckCircle, ChevronUpDown, Logout, Settings } from '@/components/Icons';
+import Button from '@/components/Button';
+import { CheckCircle, ChevronUpDown } from '@/components/Icons';
 import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 import { useProjectStore } from '@/store/project';
 import { useProjectsStore } from '@/store/projects';
@@ -16,12 +17,6 @@ const ProjectSelector = (): JSX.Element => {
   const location = useLocation();
   const projects = useAtomValue(useProjectsStore);
   const [project, setProject] = useAtom(useProjectStore);
-
-  const handleLogout = () => {
-    window.localStorage.removeItem('token');
-
-    navigate('/sign-in');
-  };
 
   const handleProjectSelect = (project: Project) => {
     setProject(project);
@@ -39,11 +34,14 @@ const ProjectSelector = (): JSX.Element => {
   return (
     <div css={tw`relative min-w-[132px]`} ref={ref}>
       <div css={tw`w-full flex items-center justify-between text-left`}>
-        <div css={tw`flex items-center`}>
+        <Link
+          to={`/projects/${project?.uuid}/servers`}
+          css={tw`h-6 flex items-center hover:bg-interactive-hover transition-colors rounded-md px-2`}
+        >
           <div css={tw`min-w-0 overflow-hidden`}>
             <h4 css={tw`font-bold`}>{project?.name}</h4>
           </div>
-        </div>
+        </Link>
 
         <button
           onClick={() => toggle(!isOpen)}
@@ -55,24 +53,26 @@ const ProjectSelector = (): JSX.Element => {
       </div>
       {isOpen && (
         <div
-          css={tw`bg-base absolute z-30 top-[calc(100% + 8px)] left-0 right-0 shadow-md rounded-md p-2`}
+          css={tw`min-w-[200px] bg-base absolute z-30 top-[calc(100% + 8px)] left-0 right-0 shadow-md ring-1 ring-black/5 rounded-md p-2`}
         >
-          <ul>
-            {projects &&
-              projects.map((item) => (
+          {projects && (
+            <ul css={tw`mb-2`}>
+              {projects.map((item) => (
                 <li key={item.uuid}>
                   <button
                     onClick={() => handleProjectSelect(item)}
                     css={tw`flex items-center justify-between w-full text-left p-2 hover:bg-interactive-hover rounded space-x-2`}
                   >
                     <span css={tw`flex flex-1 space-x-2`}>
-                      <span
+                      {/* <span
                         css={[
                           css({ backgroundColor: item.color_hex }),
                           tw`flex-shrink-0 w-5 h-5 rounded`,
                         ]}
-                      ></span>
-                      <span>{item.name}</span>
+                      ></span> */}
+                      <span css={[project?.uuid === item.uuid && tw`font-bold`]}>
+                        {item.name}
+                      </span>
                     </span>
                     {project?.uuid === item.uuid && (
                       <span css={tw`flex-shrink-0 w-5 h-5 text-green-500`}>
@@ -82,29 +82,11 @@ const ProjectSelector = (): JSX.Element => {
                   </button>
                 </li>
               ))}
-          </ul>
-          <ul>
-            <li css={tw`border-b border-black/30 my-1`}></li>
-            <li>
-              <Link
-                css={tw`flex items-center w-full text-left p-2 hover:bg-interactive-hover rounded space-x-2`}
-                to="/settings"
-              >
-                <Settings />
-                <span css={tw`font-bold`}>Settings</span>
-              </Link>
-            </li>
-            <li>
-              <button
-                css={tw`flex items-center w-full text-left p-2 hover:bg-interactive-hover rounded space-x-2`}
-                type="button"
-                onClick={() => handleLogout()}
-              >
-                <Logout />
-                <span css={tw`font-bold`}>Logout</span>
-              </button>
-            </li>
-          </ul>
+            </ul>
+          )}
+          <Button css={tw`w-full`} type="button" size="lg">
+            Create project
+          </Button>
         </div>
       )}
     </div>
