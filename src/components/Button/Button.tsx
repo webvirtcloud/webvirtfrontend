834e1d4560af13b9148690bb92434bc001d2716d
Roman Slonov <destabiliseus@gmail.com>
2022-12-25 21:04:04 +0800

    refact: button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,15 +4,39 @@ import tw from 'twin.macro';
 interface Props {
   type?: 'submit' | 'button';
   size?: 'sm' | 'md' | 'lg' | 'xl';
+  variant?: 'primary' | 'danger';
   loading?: boolean;
   disabled?: boolean;
   children: ReactNode;
+  fullWidth?: boolean;
 }
 
-// eslint-disable-next-line react/display-name
-const Button = forwardRef<HTMLButtonElement, Props>(
+const getVarianStyle = ({ variant }: { variant: Props['variant'] }) => {
+  switch (variant) {
+    case 'primary': {
+      return tw`bg-cyan-500 hover:bg-cyan-700`;
+    }
+    case 'danger': {
+      return tw`border bg-red-50 border-red-300 text-red-600`;
+    }
+    default: {
+      return tw`border hover:bg-neutral-100`;
+    }
+  }
+};
+
+export const Button = forwardRef<HTMLButtonElement, Props>(
   (
-    { children, type = 'button', size = 'xl', loading, disabled, ...rest },
+    {
+      variant,
+      children,
+      type = 'button',
+      size = 'md',
+      loading,
+      fullWidth,
+      disabled,
+      ...rest
+    },
     ref,
   ): JSX.Element => {
     return (
@@ -21,19 +45,19 @@ const Button = forwardRef<HTMLButtonElement, Props>(
         type={type}
         disabled={disabled || loading}
         css={[
-          tw`text-sm font-bold transition-colors rounded-md`,
+          tw`text-sm text-body font-bold transition-colors rounded`,
           size === 'sm'
             ? tw`h-6 px-1.5`
             : size === 'md'
-            ? tw`h-8 px-2`
+            ? tw`h-8 px-4`
             : size === 'lg'
             ? tw`h-10 px-3`
             : size === 'xl'
             ? tw`h-12 px-4`
             : tw``,
-          disabled || loading
-            ? tw`cursor-not-allowed bg-white/10`
-            : tw`bg-cyan-500 hover:bg-cyan-700`,
+          disabled || loading ? tw`cursor-not-allowed bg-white/10` : tw``,
+          getVarianStyle({ variant }),
+          fullWidth ? tw`w-full` : ``,
         ]}
         {...rest}
       >
@@ -43,4 +67,4 @@ const Button = forwardRef<HTMLButtonElement, Props>(
   },
 );
 
-export default Button;
+Button.displayName = 'Button';
