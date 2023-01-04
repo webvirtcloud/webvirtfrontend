eda9e64618475f500e6dce908fff9ca88bd52112
Roman Slonov <destabiliseus@gmail.com>
2023-01-04 21:52:02 +0800

    refactor: remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Provider as JotaiProvider } from 'jotai';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { ToastContextProvider } from '@/components/Toast/Provider';
 import { AuthLayout, DefaultLayout, ServerLayout } from '@/layouts';
