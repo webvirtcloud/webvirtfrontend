e2cd2724909caa3b734828320bdd6c822955b6dd
Roman Slonov <destabiliseus@gmail.com>
2023-03-14 22:07:55 +0800

    fix(www): add missed links in head

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -113,6 +113,13 @@ export default function Home() {
         <meta name="description" content="Self-hosted cloud platform solution" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <motion.nav
         initial={{ opacity: 0, translateY: -10 }}
