1c7eb3d3af40f2e159abe069a0f64f9be4623fb3
Roman Slonov <destabiliseus@gmail.com>
2023-03-14 18:03:22 +0800

    refactor(): improve responsiveness, add light mode

--- apps/www/components/github-button.tsx
+++ apps/www/components/github-button.tsx
@@ -3,15 +3,11 @@ import GithubIcon from '@/icons/github';
 export function GithubButton() {
   return (
     <a
-      className="group inline-flex h-12 items-center rounded-xl border border-white/50 px-4 outline-none transition-all hover:border-white/80"
+      className="group inline-flex h-12 items-center rounded-xl border border-neutral-300 px-8 outline-none transition-all duration-300 hover:scale-105 dark:border-white/50 dark:hover:border-white/80"
       href="https://github.com/retspen/webvirtmgr"
     >
       <GithubIcon className="mr-2" />
       <span>Github</span>
-      <span className="mx-4 h-full w-px bg-white/50"></span>
-      <span className="text-neutral-500 transition-colors group-hover:text-white">
-        1.928
-      </span>
     </a>
   );
 }
--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -91,6 +91,21 @@ export default function Home() {
     },
   ];
 
+  const profiles = [
+    {
+      picture: '/roman_s.jpg',
+      fullname: 'Roman Slonov',
+      github: 'https://github.com/romanslonov',
+      position: 'Front-end Developer & Designer',
+    },
+    {
+      picture: '/anatoliy_g.jpg',
+      fullname: 'Anatoliy Guskov',
+      github: 'https://github.com/retspen',
+      position: 'System Engineer',
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -103,14 +118,14 @@ export default function Home() {
         initial={{ opacity: 0, translateY: -10 }}
         animate={{ opacity: 1, translateY: 0 }}
         transition={{ delay: 0.7, duration: 0.8 }}
-        className="container mx-auto flex items-center justify-between p-8"
+        className="container mx-auto flex items-center justify-between p-4 md:p-8"
       >
         <Logotype />
-        <ul className="flex items-center space-x-8 text-neutral-500">
+        <ul className="hidden items-center space-x-8 text-neutral-600 dark:text-neutral-400 md:flex">
           <li>
             <Link
               scroll={false}
-              className="transition-colors hover:text-white"
+              className="transition-colors hover:text-black dark:hover:text-white"
               href="#features"
             >
               Features
@@ -119,7 +134,7 @@ export default function Home() {
           <li>
             <Link
               scroll={false}
-              className="transition-colors hover:text-white"
+              className="transition-colors hover:text-black dark:hover:text-white"
               href="#benefits"
             >
               Benefits
@@ -128,7 +143,7 @@ export default function Home() {
           <li>
             <Link
               scroll={false}
-              className="transition-colors hover:text-white"
+              className="transition-colors hover:text-black dark:hover:text-white"
               href="#how-it-works"
             >
               How it works
@@ -137,7 +152,7 @@ export default function Home() {
           <li>
             <Link
               scroll={false}
-              className="transition-colors hover:text-white"
+              className="transition-colors hover:text-black dark:hover:text-white"
               href="#who-we-are"
             >
               Who we are
@@ -145,12 +160,12 @@ export default function Home() {
           </li>
         </ul>
       </motion.nav>
-      <header className="container mx-auto p-8 py-32 text-center">
+      <header className="container mx-auto p-4 py-32 text-center md:p-8">
         <motion.span
           initial={{ opacity: 0, translateY: -8 }}
           animate={{ opacity: 100, translateY: 0 }}
           transition={{ delay: 0.8, duration: 0.8 }}
-          className="mb-2 inline-block rounded-lg bg-white/20 px-3 py-1.5 text-center text-xs font-bold uppercase text-white"
+          className="mb-2 inline-block rounded-lg bg-black/10 px-3 py-1.5 text-center text-xs font-bold uppercase dark:bg-white/20 dark:text-white"
         >
           Beta
         </motion.span>
@@ -158,16 +173,18 @@ export default function Home() {
           initial={{ opacity: 0, translateY: -20 }}
           animate={{ opacity: 100, translateY: 0 }}
           transition={{ duration: 1 }}
-          className="mx-auto mb-6 text-6xl font-medium leading-snug text-neutral-500"
+          className="mx-auto mb-6 text-4xl font-medium text-neutral-500 dark:text-neutral-400 md:text-6xl md:leading-snug"
         >
-          Take <span className="font-medium text-white">cloud providers</span> to <br />{' '}
-          the <span className="font-medium text-white">next level</span>.
+          Take{' '}
+          <span className="font-medium text-black dark:text-white">cloud providers</span>{' '}
+          to <br /> the{' '}
+          <span className="font-medium text-black dark:text-white">next level</span>.
         </motion.h1>
         <motion.p
           initial={{ opacity: 0, translateY: 14 }}
           animate={{ opacity: 100, translateY: 0 }}
           transition={{ delay: 0.5, duration: 1 }}
-          className="mb-12 text-xl text-neutral-100"
+          className="mb-12 text-xl dark:text-neutral-100"
         >
           Self-hosted cloud platform solution
         </motion.p>
@@ -180,14 +197,14 @@ export default function Home() {
           <Link
             scroll={false}
             href="#features"
-            className="inline-flex h-12 items-center rounded-xl bg-gradient-to-l from-blue-600 to-cyan-500 px-8 font-semibold outline-none transition-transform duration-300 hover:scale-105"
+            className="inline-flex h-12 items-center rounded-xl bg-gradient-to-l from-blue-600 to-cyan-500 px-8 font-semibold text-white outline-none transition-transform duration-300 hover:scale-105 dark:text-black"
           >
             Explore
           </Link>
           <GithubButton />
         </motion.div>
         <p className="mb-8"></p>
-        <div className="relative pt-12">
+        <div className="relative pt-20">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -250,35 +267,38 @@ export default function Home() {
           </motion.div>
         </div>
       </header>
-      <main className="container mx-auto px-8 pb-32">
+      <main className="container mx-auto px-4 pb-32 md:px-8">
         <section id="features" className="relative mx-auto max-w-6xl py-32">
           <h2 className="mb-4 text-center text-5xl font-medium">Core features</h2>
           <p className="mb-16 text-center text-2xl text-neutral-500">
             WebVirtCloud shipped with the most popular tools.
           </p>
           {/* <div className="absolute left-1/2 top-40 -z-10 h-64 w-64 -translate-x-1/2 rounded-full bg-cyan-400 blur-[80px]"></div> */}
-          <div className="grid grid-cols-3 gap-8">
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
             {features.map((feature) => (
               <div
                 key={feature.name}
                 className={cx([
-                  'rounded-xl bg-neutral-900 px-6 py-12 text-center',
+                  'rounded-xl bg-neutral-200 px-6 py-12 text-center dark:bg-neutral-800',
                   feature.coming
                     ? ''
                     : 'transition-transform duration-500 hover:scale-105',
                 ])}
               >
-                <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-lg bg-neutral-800">
-                  <feature.icon className=" h-6 w-6 text-neutral-400" />
+                <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-lg bg-neutral-300 dark:bg-neutral-700">
+                  <feature.icon className=" h-6 w-6 text-neutral-700 dark:text-neutral-400" />
                 </div>
                 <div className="flex flex-col items-center justify-center gap-2">
                   <h3
-                    className={cx(['text-xl', feature.coming ? 'text-neutral-500' : ''])}
+                    className={cx([
+                      'text-xl',
+                      feature.coming ? 'text-neutral-600 dark:text-neutral-400' : '',
+                    ])}
                   >
                     {feature.name}
                   </h3>
                   {feature.coming && (
-                    <span className="rounded-3xl bg-yellow-500/20 py-1.5 px-2 text-xs font-medium text-yellow-500">
+                    <span className="rounded-3xl bg-yellow-200/50 py-1.5 px-2 text-xs font-medium text-yellow-800 dark:bg-yellow-500/20 dark:text-yellow-500">
                       coming soon
                     </span>
                   )}
@@ -293,39 +313,39 @@ export default function Home() {
             Our web interface offers several benefits that make it the ideal tool <br />{' '}
             for managing your virtual machines.
           </p>
-          <div className="mb-4 grid grid-cols-12 gap-4">
-            <div className="relative col-span-8 flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-900 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01]">
+          <div className="mb-4 grid gap-4 md:grid-cols-12">
+            <div className="relative flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-200 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01] dark:bg-neutral-800 md:col-span-8">
               <div className="absolute -top-60 left-8 h-64 w-64 rounded-3xl bg-cyan-500 blur-[80px]"></div>
               <h3 className="relative z-10 mb-4 text-3xl font-semibold">Ease of use</h3>
-              <p className="relative z-10 text-2xl font-normal text-white/80">
+              <p className="relative z-10 text-2xl font-normal text-neutral-600 dark:text-white/80">
                 Our interface is designed to be user-friendly and intuitive, making it
                 easy for even non-technical users to manage their virtual machines.
               </p>
             </div>
-            <div className="relative col-span-4 flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-900 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01]">
+            <div className="relative flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-200 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01] dark:bg-neutral-800 md:col-span-4">
               <div className="absolute -top-20 -right-20 h-40 w-40 rounded-3xl bg-yellow-500 blur-[80px]"></div>
               <h3 className="relative z-10 mb-4 text-3xl font-semibold">Time-saving</h3>
-              <p className="relative z-10 text-2xl font-normal text-white/80">
+              <p className="relative z-10 text-2xl font-normal text-neutral-600 dark:text-white/80">
                 With our interface, you can perform virtual machine management tasks
                 quickly and easily, saving you time and effort.
               </p>
             </div>
           </div>
-          <div className="grid grid-cols-12 gap-4">
-            <div className="relative col-span-4 flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-900 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01]">
+          <div className="grid gap-4 md:grid-cols-12">
+            <div className="relative flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-200 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01] dark:bg-neutral-800 md:col-span-4">
               <div className="absolute -bottom-60 -left-20 h-64 w-64 rounded-3xl bg-emerald-500 blur-[80px]"></div>
               <h3 className="relative z-10 mb-4 text-3xl font-semibold">
                 Increased efficiency
               </h3>
-              <p className="relative z-10 text-2xl font-normal text-white/80">
+              <p className="relative z-10 text-2xl font-normal text-neutral-600 dark:text-white/80">
                 Our interface allows you to manage multiple virtual machines from a single
                 interface, which increases your efficiency and productivity.
               </p>
             </div>
-            <div className="relative col-span-8 flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-900 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01]">
+            <div className="relative flex flex-col justify-end overflow-hidden rounded-xl bg-neutral-200 p-8 pt-16 transition-transform duration-300 hover:scale-[1.01] dark:bg-neutral-800 md:col-span-8">
               <div className="absolute -bottom-56 left-8 h-64 w-64 rounded-3xl bg-pink-500 blur-[80px]"></div>
               <h3 className="relative z-10 mb-4 text-3xl font-semibold">Cost-saving</h3>
-              <p className="relative z-10 text-2xl font-normal text-white/80">
+              <p className="relative z-10 text-2xl font-normal text-neutral-600 dark:text-white/80">
                 By managing your virtual machines more efficiently, you can save on
                 resources and reduce your overall costs, because you use self-hosted
                 solution
@@ -335,7 +355,7 @@ export default function Home() {
         </section>
         <section id="how-it-works" className="relative mx-auto max-w-6xl py-32">
           <h2 className="mb-8 text-5xl font-semibold">How it works</h2>
-          <p className="mb-16 max-w-5xl text-3xl leading-relaxed text-neutral-500">
+          <p className="mb-16 max-w-5xl text-lg text-neutral-500 dark:text-neutral-400 md:text-3xl md:leading-relaxed">
             To deploy and manage VMs on this cloud platform, you need to install a
             frontend, backend, and application on the hypervisor. The frontend is
             responsible for handling user requests, providing a user interface for
@@ -348,56 +368,44 @@ export default function Home() {
         </section>
         <section id="who-we-are" className="relative py-32">
           <h2 className="mb-16 text-center text-5xl font-semibold">Who we are</h2>
-          <div className="flex justify-center gap-8">
-            <div className="rounded-3xl bg-neutral-900 p-8 text-center transition-transform hover:scale-[1.02]">
-              <Image
-                className="mb-8 h-80 w-80 rounded-3xl object-cover"
-                src="/roman_s.jpg"
-                width={800}
-                height={800}
-                alt="profile photo"
-              />
-              <div>
-                <h3 className="text-2xl">Roman Slonov</h3>
-                <p className="text-neutral-500">Front-end Developer</p>
-                <a
-                  href="https://github.com/romanslonov"
-                  target="_blank"
-                  className="mt-4 inline-block text-neutral-500 transition-colors hover:text-white"
-                >
-                  <GithubIcon />
-                </a>
+          <div className="flex flex-col justify-center gap-4 md:flex-row md:gap-8">
+            {profiles.map((profile) => (
+              <div
+                key={profile.fullname}
+                className="rounded-3xl bg-neutral-200 p-8 text-center transition-transform hover:scale-[1.02] dark:bg-neutral-800"
+              >
+                <Image
+                  className="mx-auto mb-8 h-80 w-80 rounded-3xl object-cover"
+                  src={profile.picture}
+                  width={800}
+                  height={800}
+                  alt="profile photo"
+                />
+                <div>
+                  <h3 className="text-2xl">{profile.fullname}</h3>
+                  <p className="text-neutral-600 dark:text-neutral-400">
+                    {profile.position}
+                  </p>
+                  <a
+                    href={profile.github}
+                    target="_blank"
+                    className="mt-4 inline-block text-neutral-600 transition-colors hover:text-black dark:text-neutral-400 dark:hover:text-white"
+                  >
+                    <span className="sr-only">GitHub profile</span>
+                    <GithubIcon />
+                  </a>
+                </div>
               </div>
-            </div>
-            <div className="rounded-3xl bg-neutral-900 p-8 text-center transition-transform hover:scale-[1.02]">
-              <Image
-                className="mb-8 h-80 w-80 rounded-3xl object-cover"
-                src="/anatoliy_g.jpg"
-                width={800}
-                height={800}
-                alt="profile photo"
-              />
-              <div>
-                <h3 className="text-2xl">Anatoly Guskov</h3>
-                <p className="text-neutral-500">System Engineer</p>
-                <a
-                  href="https://github.com/retspen"
-                  target="_blank"
-                  className="mt-4 inline-block text-neutral-500 transition-colors hover:text-white"
-                >
-                  <GithubIcon />
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
         </section>
         <section
-          id="who-we-are"
-          className="relative my-32 flex items-center gap-32 overflow-hidden rounded-3xl bg-neutral-900 py-32"
+          id="get-started"
+          className="relative my-32 flex flex-col overflow-hidden rounded-3xl bg-neutral-200 pt-16 pb-0 dark:bg-neutral-800 md:flex-row md:items-center md:gap-32 md:py-32"
         >
-          <div className="w-1/2 shrink-0 pl-16">
-            <h2 className="mb-8 text-5xl font-semibold">Get started</h2>
-            <p className="mb-8 text-2xl leading-relaxed text-neutral-500">
+          <div className="shrink-0 p-4 md:w-1/2 md:p-0 md:pl-16">
+            <h2 className="mb-8 text-3xl font-semibold md:text-5xl">Get started</h2>
+            <p className="mb-8 text-lg text-neutral-600 dark:text-neutral-400 md:text-2xl md:leading-relaxed">
               To get started with our web interface, all you need is a server running a
               supported operating system and libvirt installed. You can then install our
               web interface and start managing your virtual machines right away. We
@@ -406,7 +414,7 @@ export default function Home() {
             </p>
             <Link
               href="/"
-              className="inline-flex h-12 items-center rounded-xl bg-gradient-to-l from-yellow-600 to-orange-600 px-8 font-semibold outline-none transition-transform duration-300 hover:scale-105"
+              className="inline-flex h-12 items-center rounded-xl bg-gradient-to-l from-yellow-600 to-orange-600 px-8 font-semibold text-white outline-none transition-transform duration-300 hover:scale-105 dark:text-black"
             >
               Get started now
             </Link>
@@ -420,9 +428,9 @@ export default function Home() {
           ></Image>
         </section>
       </main>
-      <footer className="border-t border-neutral-800 py-8">
+      <footer className="border-t border-neutral-200 py-8 dark:border-neutral-800">
         <div className="container mx-auto space-y-4">
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col items-center justify-between gap-8 md:flex-row md:gap-0">
             <Logotype />
             <p className="text-center text-neutral-500">
               WebVirtCloud {new Date().getFullYear()}
