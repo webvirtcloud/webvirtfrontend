b0936722d5cd6f8a2e33aa99839b2186c6548afd
Roman Slonov <destabiliseus@gmail.com>
2023-03-14 22:17:24 +0800

    fix(www): github button border color on hover

--- apps/www/components/github-button.tsx
+++ apps/www/components/github-button.tsx
@@ -3,7 +3,7 @@ import GithubIcon from '@/icons/github';
 export function GithubButton() {
   return (
     <a
-      className="group inline-flex h-12 items-center rounded-xl border border-neutral-300 px-8 outline-none transition-all duration-300 hover:scale-105 dark:border-white/50 dark:hover:border-white/80"
+      className="group inline-flex h-12 items-center rounded-xl border border-neutral-300 px-8 outline-none transition-all duration-300 hover:scale-105 hover:border-neutral-500 dark:border-white/20 dark:hover:border-white/80"
       href="https://github.com/retspen/webvirtmgr"
     >
       <GithubIcon className="mr-2" />
