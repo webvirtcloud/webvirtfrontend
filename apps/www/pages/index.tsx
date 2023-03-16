8a077234d60a33997020ee0b23b176422b5304b5
Roman Slonov <destabiliseus@gmail.com>
2023-03-17 00:00:08 +0800

    fix(www): remove unused head link

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -112,7 +112,6 @@ export default function Home() {
         <title>WebVirtCloud - Take cloud providers to the next level</title>
         <meta name="description" content="Self-hosted cloud platform solution" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
