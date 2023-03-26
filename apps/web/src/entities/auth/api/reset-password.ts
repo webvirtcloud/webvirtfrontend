dafd0f044f41d248239c4a0576bc1e045319cf0a
Roman Slonov <destabiliseus@gmail.com>
2023-03-26 14:17:16 +0800

    refactor: auth entity

--- apps/web/src/entities/auth/api/reset-password.ts
+++ apps/web/src/entities/auth/api/reset-password.ts
@@ -1,4 +1,4 @@
-import request from '@/api/fetch';
+import request from '@/shared/api/request';
 
 export function resetPassword(payload: { password: string }): Promise<unknown> {
   return request.post('account/reset_passwod', { json: payload }).json();
