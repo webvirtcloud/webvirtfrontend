7012591192a81f514e19d07b447d0f03ee7f6826
Roman Slonov <destabiliseus@gmail.com>
2023-03-26 14:05:51 +0800

    refactor: remove twin macro

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,31 +2,12 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const isDevelopment = mode === 'development';
-
-  return {
-    optimizeDeps: {
-      esbuildOptions: {
-        target: 'es2020',
-      },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    plugins: [
-      react({
-        babel: { plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'] },
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
-      },
-    },
-    css: {
-      modules: {
-        generateScopedName: isDevelopment
-          ? '[name]__[local]__[hash:base64:5]'
-          : '[hash:base64:5]',
-      },
-    },
-  };
+  },
 });
