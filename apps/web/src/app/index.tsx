02667c3463e116bab42b292111cd6696e8240ac4
Roman Slonov <destabiliseus@gmail.com>
2023-03-26 14:15:11 +0800

    refactor: toaster provider

--- apps/web/src/app/index.tsx
+++ apps/web/src/app/index.tsx
@@ -1,11 +1,12 @@
 import { Routing } from '@/pages';
-
+import { Toaster } from 'ui/components/toast';
 import { withProviders } from './providers';
 
 function App() {
   return (
     <div className="antialiased">
       <Routing />
+      <Toaster />
     </div>
   );
 }
--- apps/web/src/app/providers/index.ts
+++ apps/web/src/app/providers/index.ts
@@ -2,6 +2,5 @@ import compose from 'compose-function';
 
 import { withRouter } from './with-router';
 import { withStore } from './with-store';
-import { withToasts } from './with-toasts';
 
-export const withProviders = compose(withRouter, withStore, withToasts);
+export const withProviders = compose(withRouter, withStore);
