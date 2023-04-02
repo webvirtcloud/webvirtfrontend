08d658cbd1c7357410d136e520694e42bd15571f
Roman Slonov <destabiliseus@gmail.com>
2023-04-02 13:05:08 +0800

    refactor(virtance): modify virtance's hooks to run refetch if it has status 'pending'

--- apps/web/src/entities/virtance/hooks/use-virtance.ts
+++ apps/web/src/entities/virtance/hooks/use-virtance.ts
@@ -1,18 +1,31 @@
 import useSWR from 'swr';
-import { getVirtance } from '../api';
+import { getVirtance, runVirtanceAction } from '../api';
+import type { ActionType } from '../types';
 
 export function useVirtance(id: number) {
   const {
     data: virtance,
     mutate,
     error,
-  } = useSWR(['virtance', id], () =>
-    getVirtance(id).then((response) => response.virtance),
+  } = useSWR(
+    ['virtance', id],
+    () => getVirtance(id).then((response) => response.virtance),
+    {
+      refreshInterval(latestData) {
+        return latestData?.status === 'pending' ? 1000 : 0;
+      },
+    },
   );
+
+  async function runAction(payload: ActionType) {
+    await runVirtanceAction(payload);
+    virtance && mutate({ ...virtance, status: 'pending' });
+  }
 
   return {
     virtance,
     mutate,
+    runAction,
     error,
   };
 }
--- apps/web/src/entities/virtance/hooks/use-virtances.ts
+++ apps/web/src/entities/virtance/hooks/use-virtances.ts
@@ -1,15 +1,31 @@
 import useSWR from 'swr';
-import { getVirtances } from '../api';
+import { getVirtances, runVirtanceAction } from '../api';
+import { ActionType } from '../types';
 
 export function useVirtances() {
   const {
     data: virtances,
     mutate,
     error,
-  } = useSWR('virtances', () => getVirtances().then((response) => response.virtances));
+  } = useSWR('virtances', () => getVirtances().then((response) => response.virtances), {
+    refreshInterval(latestData) {
+      return latestData?.some((virtance) => virtance.status === 'pending') ? 1000 : 0;
+    },
+  });
+
+  async function runAction(payload: ActionType) {
+    await runVirtanceAction(payload);
+    virtances &&
+      mutate(
+        virtances.map((virtance) =>
+          virtance.id === payload.id ? { ...virtance, status: 'pending' } : virtance,
+        ),
+      );
+  }
 
   return {
     virtances,
+    runAction,
     mutate,
     error,
   };
