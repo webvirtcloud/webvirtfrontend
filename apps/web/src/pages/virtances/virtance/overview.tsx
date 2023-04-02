6db78fd1a9c9f4366a852b45e1a2b1172612196a
Roman Slonov <destabiliseus@gmail.com>
2023-04-02 13:06:36 +0800

    refactor(virtance): fix colors on overview page

--- apps/web/src/pages/virtances/virtance/overview.tsx
+++ apps/web/src/pages/virtances/virtance/overview.tsx
@@ -20,21 +20,23 @@ export default function VirtanceOverview() {
         {/* <div className="rounded-xl border p-6 dark:border-neutral-700">overview</div> */}
         <div className="grid grid-cols-4 gap-4">
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Unique identifier</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">
+              Unique identifier
+            </h4>
             <p className="font-medium">{virtance?.id}</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Name</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">Name</h4>
             <p className="font-medium">{virtance?.name}</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Image</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">Image</h4>
             <p className="font-medium">
               {virtance?.image.distribution} {virtance?.image.name}
             </p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Status</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">Status</h4>
             {virtance ? (
               <p className={cx(['font-medium', VirtanceStatusStyle[virtance.status]])}>
                 {virtance.status}
@@ -50,19 +52,19 @@ export default function VirtanceOverview() {
         {/* <div className="rounded-xl border p-6 dark:border-neutral-700">overview</div> */}
         <div className="grid grid-cols-4 gap-4">
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">vCPU</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">vCPU</h4>
             <p className="font-medium">{virtance?.vcpu} core</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Memory</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">Memory</h4>
             <p className="font-medium">{virtance?.memory}</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Disk</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">Disk</h4>
             <p className="font-medium">{virtance?.disk} GB</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Size</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">Size</h4>
             <p className="font-medium">{virtance?.size.slug}</p>
           </div>
         </div>
@@ -74,15 +76,21 @@ export default function VirtanceOverview() {
         {/* <div className="rounded-xl border p-6 dark:border-neutral-700">overview</div> */}
         <div className="grid grid-cols-4 gap-4">
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Private IP</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">
+              Private IP
+            </h4>
             <p className="font-medium">{virtance?.networks.v4[0].address}</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Public IP</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">
+              Public IP
+            </h4>
             <p className="font-medium">{virtance?.networks.v4[1].address}</p>
           </div>
           <div className="space-y-1 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
-            <h4 className="font-medium dark:text-neutral-400">Compute IP</h4>
+            <h4 className="font-medium text-neutral-500 dark:text-neutral-400">
+              Compute IP
+            </h4>
             <p className="font-medium">{virtance?.networks.v4[2].address}</p>
           </div>
         </div>
