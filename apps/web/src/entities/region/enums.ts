d4c5fb14bbdcf309e1f286d53940f6638913c661
Roman Slonov <destabiliseus@gmail.com>
2023-04-03 19:20:54 +0800

    feat(region): add region images

---
+++ apps/web/src/entities/region/enums.ts
@@ -0,0 +1,4 @@
+export enum RegionCountries {
+  'nyc2' = 'usa',
+  'fra1' = 'germany',
+}
--- apps/web/src/entities/region/ui/region-card.tsx
+++ apps/web/src/entities/region/ui/region-card.tsx
@@ -1,5 +1,6 @@
 import { cx } from 'ui/lib';
 import type { Region } from '../types';
+import { RegionCountries } from '../enums';
 
 interface Props {
   isActive: boolean;
@@ -14,10 +15,22 @@ export function RegionCard({ isActive, region, onClick }: Props) {
       disabled={!region.available}
       onClick={onClick}
       className={cx([
-        'flex w-full cursor-pointer justify-between gap-4 rounded-md border p-4 disabled:cursor-not-allowed disabled:text-neutral-500 dark:border-neutral-700',
+        'flex w-full cursor-pointer items-center gap-4 rounded-md border p-4 disabled:cursor-not-allowed disabled:text-neutral-500 dark:border-neutral-700',
         isActive ? 'border-sky-500 ring-1 ring-sky-500' : '',
       ])}
     >
+      <img
+        src={
+          new URL(
+            `/src/shared/assets/images/countries/${RegionCountries[region.slug]}.svg`,
+            import.meta.url,
+          ).href
+        }
+        className={cx(!region.available && 'grayscale')}
+        width={32}
+        height={32}
+        alt=""
+      />
       <div className="font-medium">{region.name}</div>
     </button>
   );
