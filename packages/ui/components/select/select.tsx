f3c8ebcaa86962cf725edc63dce122c9a7f4c1b4
Roman Slonov <destabiliseus@gmail.com>
2023-04-08 02:19:31 +0800

    refactor(select): change appearance

--- packages/ui/components/select/select.tsx
+++ packages/ui/components/select/select.tsx
@@ -15,7 +15,7 @@ export const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cx(
-      'flex h-10 w-full items-center justify-between rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
+      'flex h-8 w-full items-center justify-between rounded-lg border border-neutral-300 bg-neutral-50 px-3 py-2 text-sm transition-all focus:border-sky-500 focus:bg-transparent focus:outline-none focus:ring-2 focus:ring-sky-500/30 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-600 dark:bg-neutral-800',
       className,
     )}
     {...props}
@@ -34,7 +34,7 @@ export const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cx(
-        'animate-in fade-in-80 relative z-50 min-w-[8rem] overflow-hidden rounded-md border border-slate-100 bg-white text-slate-700 shadow-md dark:border-slate-800 dark:bg-slate-800 dark:text-slate-400',
+        'animate-in fade-in-80 relative z-50 min-w-[8rem] overflow-hidden rounded-md border border-neutral-100 bg-white text-neutral-700 shadow-md dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-400',
         className,
       )}
       {...props}
@@ -52,7 +52,7 @@ export const SelectLabel = React.forwardRef<
   <SelectPrimitive.Label
     ref={ref}
     className={cx(
-      'py-1.5 pr-2 pl-8 text-sm font-semibold text-slate-900 dark:text-slate-300',
+      'py-1.5 pl-8 pr-2 text-sm font-semibold text-neutral-900 dark:text-neutral-300',
       className,
     )}
     {...props}
@@ -67,7 +67,7 @@ export const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cx(
-      'relative flex cursor-default select-none items-center rounded-sm py-1.5 pr-2 pl-8 text-sm font-medium outline-none focus:bg-slate-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-700',
+      'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm font-medium outline-none focus:bg-neutral-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-neutral-700',
       className,
     )}
     {...props}
@@ -89,7 +89,7 @@ export const SelectSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Separator
     ref={ref}
-    className={cx('-mx-1 my-1 h-px bg-slate-100 dark:bg-slate-700', className)}
+    className={cx('-mx-1 my-1 h-px bg-neutral-100 dark:bg-neutral-700', className)}
     {...props}
   />
 ));
