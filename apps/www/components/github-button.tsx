ab95a33ce651a6f1a3d0d0d1526e19ab211178b7
Roman Slonov <destabiliseus@gmail.com>
2023-08-29 22:43:45 +0800

    refactor(github-button): update link

--- apps/www/components/github-button.tsx
+++ apps/www/components/github-button.tsx
@@ -4,7 +4,7 @@ export function GithubButton() {
   return (
     <a
       className="group inline-flex h-12 items-center rounded-xl border border-neutral-300 px-8 outline-none transition-all duration-300 hover:scale-105 hover:border-neutral-500 dark:border-white/20 dark:hover:border-white/80"
-      href="https://github.com/retspen/webvirtmgr"
+      href="https://github.com/webvirtcloud/"
     >
       <GithubIcon className="mr-2" />
       <span>Github</span>
