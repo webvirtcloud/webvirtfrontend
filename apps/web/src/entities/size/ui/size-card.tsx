f05f4e8ae672596fc7e656e2a8f01e0074565fa8
Roman Slonov <destabiliseus@gmail.com>
2023-09-17 21:14:08 +0800

    refactor(size-card): swap desciption and price

--- apps/web/src/entities/size/ui/size-card.tsx
+++ apps/web/src/entities/size/ui/size-card.tsx
@@ -21,8 +21,8 @@ export function SizeCard({ size, isActive, isDisabled, onClick }: Props) {
     >
       <div className="flex flex-1 justify-between gap-8">
         <div className="flex w-24 flex-col items-center justify-center border-r dark:border-neutral-700">
-          <div className="text-xl font-medium">{size.price_monthly}$</div>
-          <div className="text-xs text-neutral-500">{size.description}</div>
+          <div className="text-xl font-medium">{size.description}</div>
+          <div className="text-xs text-neutral-500">${size.price_monthly}</div>
         </div>
         <ul className="space-y-2 text-end">
           <li>
