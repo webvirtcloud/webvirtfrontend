46cbc543a4aaecc98afca4264f0cd730d99bcbab
Roman Slonov <destabiliseus@gmail.com>
2023-11-17 02:01:28 +0700

    fix(web): add slashes before domain

--- apps/web/src/shared/constants.ts
+++ apps/web/src/shared/constants.ts
@@ -1,3 +1,2 @@
-export const API_DOMAIN = import.meta.env.VITE_API_DOMAIN;
+export const API_DOMAIN = `//${import.meta.env.VITE_API_DOMAIN}`;
 export const API_PREFIX = import.meta.env.VITE_API_PREFIX;
-
