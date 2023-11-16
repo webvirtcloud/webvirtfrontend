6cdffb0eabaf2902843d78e2597bbfcc6f3dbf9a
Roman Slonov <destabiliseus@gmail.com>
2023-11-16 21:09:39 +0700

    fix(web): populate API_DOMAIN env

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,25 +1,22 @@
 import react from '@vitejs/plugin-react';
-import dotenv, { type DotenvPopulateInput } from 'dotenv';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-const config = dotenv.config({
-  path: resolve(__dirname, `.env.${process.env.NODE_ENV}`),
-});
-
-// WORKAROUND: We need to populate the process.env object with `API_DOMAIN` variable from docker
-//env variables with VITE_ prefix to make them available in the browser.
-dotenv.populate(process.env as DotenvPopulateInput, {
-  VITE_API_BASE_DOMAIN: (process.env.API_DOMAIN || config.parsed?.API_DOMAIN) as string,
-});
-
-// console.log(process.env);
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_API_BASE_DOMAIN': JSON.stringify(
+        process.env.API_DOMAIN || env.API_DOMAIN,
+      ),
     },
-  },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+  };
 });
