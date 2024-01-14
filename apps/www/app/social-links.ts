5ef545bc0951b23b080a98b6168bb5624c924ff9
Roman Slonov <destabiliseus@gmail.com>
2024-01-15 00:08:21 +0800

    feat(www): add discord link

--- apps/www/app/social-links.ts
+++ apps/www/app/social-links.ts
@@ -1,6 +1,7 @@
 import GithubIcon from '@/icons/github';
 import TgIcon from '@/icons/tg';
 import XIcon from '@/icons/x';
+import Discord from '@/icons/discord';
 
 export const SOCIAL_LINKS = [
   {
@@ -17,5 +18,10 @@ export const SOCIAL_LINKS = [
     icon: TgIcon,
     name: 'telegram',
     href: 'https://t.me/webvirtcloud',
+  },
+  {
+    icon: Discord,
+    name: 'discord',
+    href: 'https://discord.gg/aWuTAVRxBB',
   },
 ] as const;
