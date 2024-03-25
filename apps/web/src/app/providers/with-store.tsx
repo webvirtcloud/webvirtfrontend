201136dd6972ee4a6713cc44a7d629a22cd15622
Roman Slonov <destabiliseus@gmail.com>
2024-03-25 17:35:19 +0800

    refactor: remove jotai

--- apps/web/src/app/providers/with-store.tsx
+++
@@ -1,9 +0,0 @@
-import { Provider as JotaiProvider } from 'jotai';
-import { ComponentType } from 'react';
-
-export const withStore = (Component: ComponentType) => () =>
-  (
-    <JotaiProvider>
-      <Component />
-    </JotaiProvider>
-  );
