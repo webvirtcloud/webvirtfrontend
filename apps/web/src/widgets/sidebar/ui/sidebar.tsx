900d4294c7cbe5af287020eb7928b46876090209
Roman Slonov <destabiliseus@gmail.com>
2024-04-07 14:28:56 +0800

    fix: sidebar

--- apps/web/src/widgets/sidebar/ui/sidebar.tsx
+++ apps/web/src/widgets/sidebar/ui/sidebar.tsx
@@ -23,7 +23,7 @@ export function Sidebar({ className }: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cx([
-        'bg-background fixed top-0 z-10 flex h-screen w-56 transform flex-col gap-8 self-start border-r p-4 transition-transform lg:sticky lg:translate-x-0',
+        'fixed top-0 z-10 flex h-screen w-56 transform flex-col gap-8 self-start border-r bg-white p-4 transition-transform lg:sticky lg:translate-x-0 dark:border-neutral-700 dark:bg-neutral-900',
         className,
         isSidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0',
       ])}
@@ -31,7 +31,7 @@ export function Sidebar({ className }: HTMLAttributes<HTMLDivElement>) {
       <Button
         onClick={() => closeSidebar()}
         className={cx([
-          'text-muted-foreground absolute -right-10 top-5 h-7 w-7 transition-all lg:pointer-events-none',
+          'absolute -right-10 top-5 h-7 w-7 px-0 transition-all lg:pointer-events-none',
           isSidebarOpen
             ? 'translate-x-0 opacity-100 lg:-translate-x-12 lg:opacity-0'
             : '-translate-x-12 opacity-0',
