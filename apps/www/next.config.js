f890972060a8363d99ac3d9170c3bf0dc0e01327
Roman Slonov <destabiliseus@gmail.com>
2024-04-08 21:39:08 +0800

    chore: add redirect

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -4,6 +4,15 @@ const { withContentlayer } = require('next-contentlayer')
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async redirects () {
+    return [
+      {
+        source: '/docs/getting-started/setup',
+        destination: '/docs/introduction',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = withContentlayer(nextConfig)
