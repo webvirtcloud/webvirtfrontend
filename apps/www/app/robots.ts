46a6302bd76aa86df4763ef2c1b4032a26291dc9
Roman Slonov <destabiliseus@gmail.com>
2024-04-28 10:37:28 +0800

    refactor: update robots, add sitemap

---
+++ apps/www/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+import { baseUrl } from 'app/sitemap';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
---
+++ apps/www/app/sitemap.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from 'next';
+import { compareDesc } from 'date-fns';
+import { allPosts } from '@/.contentlayer/generated';
+
+export const baseUrl = 'https://webvirt.cloud';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = allPosts
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+    .map((post) => ({
+      url: `${baseUrl}/blog/${post.slug}`,
+      lastModified: post.date,
+    }));
+
+  let routes = [
+    '',
+    '/blog',
+    '/docs',
+    '/docs/introduction',
+    '/docs/installation/setup-controller',
+    '/docs/installation/setup-hypervisor',
+  ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  return [...routes, ...posts];
+}
