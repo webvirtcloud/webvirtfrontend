c351495532e3b6bea819ff3cdbe4e96f860fd0e1
Roman Slonov <destabiliseus@gmail.com>
2024-10-12 16:00:06 +0800

    refator: logotype and font

--- apps/www/components/logotype.tsx
+++ apps/www/components/logotype.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 export function Logotype() {
   return (
-    <Link href="/" className="flex items-center space-x-4">
+    <Link href="/" className="flex items-center space-x-2">
       <Image
-        width={48}
-        height={48}
-        className="h-10 w-10"
+        width={32}
+        height={32}
+        className="h-8 w-8"
         src="/logo.svg"
         alt="WebVirtCloud logotype"
       />
-      <span className="text-lg font-medium">WebVirtCloud</span>
+      <span className="text-lg font-semibold">WebVirtCloud</span>
     </Link>
   );
 }
