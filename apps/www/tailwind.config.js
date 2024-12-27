bb8d31bd23c3133dadaafb9874f519cfc1de6030
Roman Slonov <destabiliseus@gmail.com>
2024-12-28 01:19:13 +0800

    refactor(www): adjust tw typography theme

--- apps/www/tailwind.config.js
+++ apps/www/tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   presets: [require('ui/tailwind.preset')],
   darkMode: 'class',
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "../../packages/ui/components/**/*.{ts,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/components/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
@@ -14,43 +14,70 @@ module.exports = {
         '8xl': '90rem',
       },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+          DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+          foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
         },
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
         },
       },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              backgroundColor: 'var(--tw-prose-code-bg)',
+              boxShadow: 'inset 0 0 0 1px var(--tw-prose-code-ring)',
+              padding: '.25rem .375rem',
+              fontWeight: 600,
+              fontSize: '.75rem',
+              borderRadius: '6px',
+              borderWidth: '1px',
+            },
+            a: {
+              textDecoration: 'none',
+              '&:hover': {
+                color: theme('colors.sky.600'),
+                textDecoration: 'underline',
+                textUnderlineOffset: '4px',
+              },
+            },
+            'code::before': {
+              content: '"',
+            },
+            'code::after': {
+              content: '"',
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [
-    require('@tailwindcss/typography', require("tailwindcss-animate")),
-  ],
-}
+  plugins: [require('@tailwindcss/typography', require('tailwindcss-animate'))],
+};
