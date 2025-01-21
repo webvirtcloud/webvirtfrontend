bb70eb71e9459fca90f6720dd3b03081a0eb03e6
Roman Slonov <destabiliseus@gmail.com>
2025-01-22 04:51:01 +0800

    fix: API_DOMAIN constant

--- apps/web/src/shared/constants.ts
+++ apps/web/src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const API_DOMAIN = import.meta.env.VITE_API_DOMAIN || window.location.href;
+export const API_DOMAIN = import.meta.env.VITE_API_DOMAIN || window.location.origin;
 export const API_PREFIX = import.meta.env.VITE_API_PREFIX;
 export const REFRESH_INTERVAL = 1000;
 export const IS_LOADBALANCER_ENABLED = import.meta.env.VITE_LOADBALANCER === 'true';
