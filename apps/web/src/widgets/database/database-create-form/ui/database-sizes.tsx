b9b3017964e51acc35c627e166198a0bfa177d02
Roman Slonov <destabiliseus@gmail.com>
2025-05-08 17:30:18 +0800

    refactor(database): add cards to sizes

--- apps/web/src/widgets/database/database-create-form/ui/database-sizes.tsx
+++ apps/web/src/widgets/database/database-create-form/ui/database-sizes.tsx
@@ -1,14 +1,8 @@
 import { useEffect, useMemo } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from 'ui/components/select';
 
-import { Dbm } from '@/entities/database';
+import type { Dbm } from '@/entities/database';
+import { SizeCard } from '@/entities/size';
 
 import { DatabaseCreateForm } from '../types';
 
@@ -27,42 +21,33 @@ export function DatabaseSizes({ engines }: { engines?: Record<string, Dbm[]> }) 
     form.resetField('size');
   }, [version]);
 
-  return (
-    <div className="space-y-4">
-      <div>
-        <h2 className="text-lg font-semibold">Size</h2>
-      </div>
-      <div className="grid gap-2 md:max-w-64">
-        <Controller
-          control={form.control}
-          name="size"
-          render={({ field }) => (
-            <Select
-              onValueChange={field.onChange}
-              value={field.value}
-              disabled={sizes.length === 0}
-            >
-              <SelectTrigger>
-                <SelectValue
-                  placeholder={
-                    sizes.length === 0 ? 'No sizes available' : 'Select a size'
-                  }
+  return sizes ? (
+    <section className="mb-12">
+      <h2 className="mb-4 text-lg font-semibold">Sizes</h2>
+      {sizes.length ? (
+        <div className="grid gap-4 md:grid-cols-3">
+          {sizes.map((size) => (
+            <Controller
+              key={size.slug}
+              name="size"
+              control={form.control}
+              render={({ field }) => (
+                <SizeCard
+                  onClick={() => field.onChange(size.slug)}
+                  isActive={field.value === size.slug}
+                  isDisabled={!size.available}
+                  key={size.slug}
+                  size={size}
                 />
-              </SelectTrigger>
-              <SelectContent>
-                {sizes.map((size) => (
-                  <SelectItem key={size.slug} value={size.slug}>
-                    {size.slug}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          )}
-        />
-        {form.formState.errors.size ? (
-          <p className="text-red-500">{form.formState.errors.size.message}</p>
-        ) : null}
-      </div>
-    </div>
-  );
+              )}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="flex max-w-sm flex-col items-center gap-2 rounded-md border border-dashed p-8">
+          You don&apos;t have sizes in selected engine or version.
+        </p>
+      )}
+    </section>
+  ) : null;
 }
